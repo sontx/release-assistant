5227d1f6019b9448e0807e2391a3e01df33b653b
sontx <xuanson33bk@gmail.com>
2016-11-30 12:06:45 +0700

    + textbox with some extra feature(likes hint)

---
+++ XTextBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ReleaseAssistant
+{
+    class XTextBox : TextBox
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
+        private string _hint;
+        
+        [Browsable(true)]
+        public string Hint
+        {
+            get { return _hint; }
+            set
+            {
+                _hint = value;
+                SendMessage(Handle, 0x1501, 1, _hint);
+            }
+        }
+    }
+}
